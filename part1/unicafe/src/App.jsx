2ea2780c6e8d2001a40d806770972ba752df2007
Iván Guerrero <ivangvprogrammer@gmail.com>
2023-10-17 15:19:01 -0500

    [ADD] exercise 1.7 and 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,17 +8,25 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAllClicks] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAllClicks(allClicks + 1)
+    setAverage(average + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAllClicks(allClicks + 1)
+    setAverage(average + 0)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setAllClicks(allClicks + 1)
+    setAverage(average - 1)
   }
 
   return (
@@ -27,7 +35,7 @@ function App() {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks} average={average}/>
     </div>
   )
 }
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,10 +1,13 @@
-const  Statistics = ({ good, neutral, bad}) => {
+const  Statistics = ({ good, neutral, bad, allClicks, average}) => {
     return(
         <div>
             <h2>Statistics</h2>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {allClicks}</p>
+            <p>average {(average) / 3}</p>
+            <p>{ good > 0 ? `positive ${(good / allClicks) * 100}%` : 'No hay comentarios buenos'}</p>
         </div>
     )
 }
