75a3eb29e98054a5a2e921701b7214e7c6da9ebe
Iván Guerrero <ivangvprogrammer@gmail.com>
2023-10-17 14:27:24 -0500

    [ADD] unicafe, exercise 1.6

---
+++ part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,12 @@
+const  Statistics = ({ good, neutral, bad}) => {
+    return(
+        <div>
+            <h2>Statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </div>
+    )
+}
+
+export default Statistics;
