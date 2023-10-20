9eb150deb4e93092f7041001c258eb22686188c7
Iván Guerrero <ivangvprogrammer@gmail.com>
2023-10-19 21:10:50 -0500

    [UPDATE] exercise 1.11 creating a table

--- part1/unicafe/src/components/StatisticLine.jsx
+++ part1/unicafe/src/components/StatisticLine.jsx
@@ -1,8 +1,11 @@
 const StatisticsLine = ({ text, value }) => {
     return(
-        <p>
-            {text} {value}
-        </p>
+        <table style={{width: '100%', borderCollapse: 'collapse', textAlign: 'center'}}>
+            <tr>
+                <td style={{padding: '3px'}}>{text}</td>
+                <td style={{ width:'50%'}}>{value}</td>
+            </tr>
+        </table>
     )
 }
 
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -10,8 +10,8 @@ const  Statistics = ({ good, neutral, bad, allClicks, average}) => {
                     <StatisticsLine text='neutral' value={neutral}/>
                     <StatisticsLine text='bad' value={bad}/>
                     <StatisticsLine text='all' value={allClicks}/>
-                    <StatisticsLine text='average' value={(average) / 3}/>
-                    <StatisticsLine text='' value={good > 0 ? `positive ${(good / allClicks) * 100}%` : ''}/>
+                    <StatisticsLine text='average' value={((average) / 3).toFixed(1)}/>
+                    <StatisticsLine text='positive' value={good > 0 ? `${((good / allClicks) * 100).toFixed(1)}%` : ''}/>
                 </div>
             ) : (
                 <p>No feedback give</p>
