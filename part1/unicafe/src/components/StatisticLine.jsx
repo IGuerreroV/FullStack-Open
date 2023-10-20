217b5339ea74f6452b3fd163535a0edec90275c5
Iván Guerrero <ivangvprogrammer@gmail.com>
2023-10-20 15:25:19 -0500

    [UPDATE] add tbody to the table

--- part1/unicafe/src/components/StatisticLine.jsx
+++ part1/unicafe/src/components/StatisticLine.jsx
@@ -1,10 +1,12 @@
 const StatisticsLine = ({ text, value }) => {
     return(
         <table style={{width: '100%', borderCollapse: 'collapse', textAlign: 'center'}}>
-            <tr>
-                <td style={{padding: '3px'}}>{text}</td>
-                <td style={{ width:'50%'}}>{value}</td>
-            </tr>
+            <tbody>
+                <tr>
+                    <td style={{padding: '3px'}}>{text}</td>
+                    <td style={{ width:'50%'}}>{value}</td>
+                </tr>
+            </tbody>
         </table>
     )
 }
